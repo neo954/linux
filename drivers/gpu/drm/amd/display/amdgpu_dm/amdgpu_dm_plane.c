b25f2f035ce939cb7c72ba3fedbf4bcff91ed3c0
Bouke Sybren Haarsma <boukehaarsma23@gmail.com>
2023-12-15 11:14:58 +0100

    Don't create color_mgmt_properties on asics < SIENNA_CICHLID

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
@@ -1748,7 +1748,8 @@ int amdgpu_dm_plane_init(struct amdgpu_display_manager *dm,
 	drm_plane_helper_add(plane, &dm_plane_helper_funcs);
 
 #ifdef CONFIG_DRM_AMD_COLOR_STEAMDECK
-	dm_atomic_plane_attach_color_mgmt_properties(dm, plane);
+	if (dm->adev->asic_type >= CHIP_SIENNA_CICHLID)
+		dm_atomic_plane_attach_color_mgmt_properties(dm, plane);
 #endif
 	/* Create (reset) the plane state */
 	if (plane->funcs->reset)
