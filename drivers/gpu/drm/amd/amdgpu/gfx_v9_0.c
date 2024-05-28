5ae642e9f436a6a5def8ccadd6303a3096e9cdf4
fewtarius <88717793+fewtarius@users.noreply.github.com>
2024-05-28 18:54:32 +0200

    Add Atari VCS quirk

--- drivers/gpu/drm/amd/amdgpu/gfx_v9_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v9_0.c
@@ -1170,6 +1170,8 @@ static const struct amdgpu_gfxoff_quirk amdgpu_gfxoff_quirk_list[] = {
 	{ 0x1002, 0x15dd, 0x103c, 0x83e7, 0xd3 },
 	/* GFXOFF is unstable on C6 parts with a VBIOS 113-RAVEN-114 */
 	{ 0x1002, 0x15dd, 0x1002, 0x15dd, 0xc6 },
+	/* GFXOFF is unstable on 91 (Atari VCS) with 113-RAVEN2-117 */
+	{ 0x1002, 0x15D8, 0x1002, 0x15D8, 0x91 },
 	/* Apple MacBook Pro (15-inch, 2019) Radeon Pro Vega 20 4 GB */
 	{ 0x1002, 0x69af, 0x106b, 0x019a, 0xc0 },
 	{ 0, 0, 0, 0, 0 },
