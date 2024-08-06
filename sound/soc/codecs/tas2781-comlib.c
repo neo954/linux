d065bf840aa6eaba8f517001a38065367e6beae9
GONG Jie <gongjie.jie@gmail.com>
2024-08-06 17:28:02 +0800

    ALSA: hda/ASOC tas2781: align the path of firmware
    TI upstream the files in ti/tas2781 for linux-firmware.
    Make the driver find the correct files.
    
    Signed-off-by: Aaron Ma <aaron.ma@canonical.com>

--- sound/soc/codecs/tas2781-comlib.c
+++ sound/soc/codecs/tas2781-comlib.c
@@ -277,7 +277,7 @@ int tascodec_init(struct tasdevice_priv *tas_priv, void *codec,
 	 */
 	mutex_lock(&tas_priv->codec_lock);
 
-	scnprintf(tas_priv->rca_binaryname, 64, "%sRCA%d.bin",
+	scnprintf(tas_priv->rca_binaryname, 64, "%s%sRCA%d.bin", TAS2781_FIRMWARE_ROOT,
 		tas_priv->dev_name, tas_priv->ndev);
 	crc8_populate_msb(tas_priv->crc8_lkp_tbl, TASDEVICE_CRC8_POLYNOMIAL);
 	tas_priv->codec = codec;
