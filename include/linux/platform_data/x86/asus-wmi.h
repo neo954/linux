41446babc7a89479b3bd1495d4b4b441d817ae76
Luke D. Jones <luke@ljones.dev>
2024-09-22 21:40:46 +1200

    platform/x86: asus-wmi: export symbols used for read/write WMI
    Export some rather helpful read/write WMI symbols using a namespace.
    These are DEVS and DSTS only, or require the arg0 input.
    
    Also does a slight refactor of internals of these functions.
    
    Signed-off-by: Luke D. Jones <luke@ljones.dev>
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>

--- include/linux/platform_data/x86/asus-wmi.h
+++ include/linux/platform_data/x86/asus-wmi.h
@@ -159,9 +159,19 @@
 
 #if IS_REACHABLE(CONFIG_ASUS_WMI)
 void set_ally_mcu_hack_available(bool enabled);
+int asus_wmi_get_devstate_dsts(u32 dev_id, u32 *retval);
+int asus_wmi_set_devstate(u32 dev_id, u32 ctrl_param, u32 *retval);
 int asus_wmi_evaluate_method(u32 method_id, u32 arg0, u32 arg1, u32 *retval);
 #else
 static inline void set_ally_mcu_hack_available(bool enabled) {
+	return -ENODEV;
+}
+static inline int asus_wmi_get_devstate_dsts(u32 dev_id, u32 *retval)
+{
+	return -ENODEV;
+}
+static inline int asus_wmi_set_devstate(u32 dev_id, u32 ctrl_param, u32 *retval)
+{
 	return -ENODEV;
 }
 static inline int asus_wmi_evaluate_method(u32 method_id, u32 arg0, u32 arg1,
