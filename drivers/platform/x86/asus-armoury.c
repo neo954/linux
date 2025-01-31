f68414780b05bcfe562595171d930afec0c261a8
Luke Jones <luke@ljones.dev>
2025-02-01 11:28:50 +1300

    backport: asus-armoury: fix fw_attributes_class after cherry-pick from wip/ally-master
    Signed-off-by: Luke Jones <luke@ljones.dev>

--- drivers/platform/x86/asus-armoury.c
+++ drivers/platform/x86/asus-armoury.c
@@ -62,6 +62,8 @@ enum cpu_core_value {
 
 static struct asus_wmi_armoury_interface wmi_armoury_interface;
 
+static const struct class *fw_attr_class;
+
 static struct asus_armoury_priv asus_armoury = {
 	.mutex = __MUTEX_INITIALIZER(asus_armoury.mutex)
 };
@@ -876,8 +878,13 @@ static int asus_fw_attr_add(void)
 	const char *name;
 	int err, i;
 
-	asus_armoury.fw_attr_dev = device_create(&firmware_attributes_class, NULL, MKDEV(0, 0),
+	err = fw_attributes_class_get(&fw_attr_class);
+	if (err)
+		return err;
+
+	asus_armoury.fw_attr_dev = device_create(fw_attr_class, NULL, MKDEV(0, 0),
 						NULL, "%s", DRIVER_NAME);
+
 	if (IS_ERR(asus_armoury.fw_attr_dev)) {
 		err = PTR_ERR(asus_armoury.fw_attr_dev);
 		goto fail_class_get;
@@ -994,8 +1001,9 @@ err_remove_file:
 err_destroy_kset:
 	kset_unregister(asus_armoury.fw_attr_kset);
 err_destroy_classdev:
+	device_destroy(fw_attr_class, MKDEV(0, 0));
 fail_class_get:
-	device_destroy(&firmware_attributes_class, MKDEV(0, 0));
+	fw_attributes_class_put();
 	return err;
 }
 
@@ -1136,7 +1144,8 @@ static void __exit asus_fw_exit(void)
 
 	sysfs_remove_file(&asus_armoury.fw_attr_kset->kobj, &pending_reboot.attr);
 	kset_unregister(asus_armoury.fw_attr_kset);
-	device_destroy(&firmware_attributes_class, MKDEV(0, 0));
+	device_destroy(fw_attr_class, MKDEV(0, 0));
+	fw_attributes_class_put();
 
 	asus_wmi_unregister_armoury_interface(&wmi_armoury_interface);
 
