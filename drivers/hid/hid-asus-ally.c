e989794c707d0ba7525238b5bf4e8a560da0241f
Luke Jones <luke@ljones.dev>
2025-02-01 14:19:33 +1300

    backport: asus-wmi: fix symbol 'ASUS_WMI' after cherry-pick from wip/ally-master
    Signed-off-by: Luke Jones <luke@ljones.dev>

--- drivers/hid/hid-asus-ally.c
+++ drivers/hid/hid-asus-ally.c
@@ -2256,7 +2256,7 @@ static void __exit rog_ally_exit(void)
 module_init(rog_ally_init);
 module_exit(rog_ally_exit);
 
-MODULE_IMPORT_NS("ASUS_WMI");
+MODULE_IMPORT_NS(ASUS_WMI);
 MODULE_AUTHOR("Luke D. Jones");
 MODULE_DESCRIPTION("HID Driver for ASUS ROG Ally gamepad configuration.");
 MODULE_LICENSE("GPL");
